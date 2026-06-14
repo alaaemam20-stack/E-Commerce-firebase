4311ab4e13a464c65491a05438ee8b7613341328
Alaa Emam <aslamamam609@gmail.com>
2026-06-14 21:42:08 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:ecommerce_app_api_26/features/auth/presentation/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:ecommerce_app_api_26/core/theme/app_theme.dart';
+
+import 'features/auth/presentation/screens/signup_screen.dart';
+import 'features/main_wrapper/presentation/screens/main_wrapper.dart';
+import 'firebase_options.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
+      home: const LoginScreen(),
+    );
+  }
+}
